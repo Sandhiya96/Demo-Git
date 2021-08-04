b90616aa5cde03adfdfcf2caf54d31c9d885c1a2
Sandhiya96 <sandhuajap@gmail.com>
2021-08-04 15:04:17 +0530

    first commit

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Employee {
+	private void empId() {
+		System.out.println("emp Id:12");
+	}
+	private void empName() {
+         System.out.println("Emp name:dini");
+	}
+
+}
